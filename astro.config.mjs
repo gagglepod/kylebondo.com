42383e341f5e8c0fda618c381824106822ff0168
gagglepod <gagglepod@gmail.com>
2025-01-12 18:31:21 -0600

    Update for 2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,30 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import mdx from '@astrojs/mdx'
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+import remarkGfm from "remark-gfm";
+import remarkSmartypants from "remark-smartypants";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
+  site: "https://astro-blog-template.netlify.app",
   integrations: [mdx(), svelte()],
+  server: {
+    port: 4000,
+  },
   markdown: {
     shikiConfig: {
-      theme: 'nord',
+      theme: "nord",
     },
-    remarkPlugins: ['remark-gfm', 'remark-smartypants'],
+    remarkPlugins: [remarkGfm, remarkSmartypants], // Explicitly imported plugins
     rehypePlugins: [
       [
-        'rehype-external-links',
+        rehypeExternalLinks,
         {
-          target: '_blank',
+          target: "_blank",
+          rel: ["noopener", "noreferrer"], // Recommended additional attributes
         },
       ],
     ],
   },
-})
+});
